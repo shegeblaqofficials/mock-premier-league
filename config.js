944197dfbb4f33882aa08c4916984e2613f54ed4
Olaosebikan Segun <olaosebikan4officials@gmail.com>
2020-01-20 13:46:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export const dbUrl = 'mongodb+srv://shegeblaq:Marshal05@cluster0-mukvv.mongodb.net/MockPremierLeagueDb?retryWrites=true&w=majority'
-//export const dbUrl = 'mongodb://localhost/MPLdb'
+//export const dbUrl = 'mongodb://localhost/MockPremierLeagueDb'
 export const port = 4000
 
-///TEST VARIABLE
-export const fixtureTestId = '5e24799b8d6e1f6804cb72a6';
-export const teamTestId = '5e1dbfe90e76370f7c0b1077';
+///TEST VARIABLE SHOULD BE PRESENT IN THE DATABASE
+export const fixtureTestId = '5e259b3f31f293002319c385';
+export const teamTestId = '5e2598ba31f293002319c37f';
 
-//TEST USER AND ADMIN CREDENTIAL
-export const userTestToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoZWdlYmxhcUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6InNoZWdlYmxhcUBnbWFpbC5jb20iLCJfaWQiOiI1ZTFlZjZkMjYxYjk2MTU1ODA5YWJkMWEiLCJyb2xlIjoidXNlciIsImlhdCI6MTU3OTA4NzYzM30.1hJon3cGzh53BBtif29IvxNYMF-8JWro3oe8713wNxM'
-export const adminTestToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoZWdlYmxhcTA1QGdtYWlsLmNvbSIsInVzZXJuYW1lIjoic2hlZ2VibGFxMDVAZ21haWwuY29tIiwiX2lkIjoiNWUxZWY3OTQ2MWI5NjE1NTgwOWFiZDFiIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTc5MDg3NzgyfQ.XmJAzyWhWh-pCumN7AdNy1Wz9G4XLa0gpY0UedzhSsE'
+//TEST USER AND ADMIN CREDENTIAL | SHOULD BE REGISTERED USERS
+export const userTestToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoZWdlYmxhcUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6InNoZWdlYmxhcUBnbWFpbC5jb20iLCJfaWQiOiI1ZTI1OTU3MjMxZjI5MzAwMjMxOWMzN2QiLCJyb2xlIjoidXNlciIsImlhdCI6MTU3OTUyMTczOX0.5AQCCKPqA40v10mGPNPjgv88JuQ440PK1aM7Q_ZT_eg'
+export const adminTestToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoZWdlYmxhcTA1QGdtYWlsLmNvbSIsInVzZXJuYW1lIjoic2hlZ2VibGFxMDVAZ21haWwuY29tIiwiX2lkIjoiNWUyNTk1Y2YzMWYyOTMwMDIzMTljMzdlIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTc5NTIxNTMxfQ.MXspV8-R-nrLHGbLrFbF5E-D5HDKRuijEgX4RpHGy1w'
